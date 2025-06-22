2f16934408ac3a3c358dc1129b6f1fbb018c3b14
shibambms27 <150105056+shibambms27@users.noreply.github.com>
2025-06-23 02:31:19 +0530

    Update and rename default.xml to default.sh

---
+++ default.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo "Cloning device stuffs"
+
+git lfs clone git@github.com:LineageOS/android_device_xiaomi_zeus.git -b lineage-22.2 device/xiaomi/zeus
+
+git lfs clone git@github.com:LineageOS/android_device_xiaomi_sm8450-common.git -b lineage-22.2 device/xiaomi/sm8450-common
+
+rm -rf hardware/xiaomi
+
+git lfs clone git@github.com:LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+
+git lfs clone git@github.com:TheMuppets/proprietary_vendor_xiaomi_sm8450-common.git -b lineage-22.2 vendor/xiaomi/sm8450-common
+
+git lfs clone git@github.com:TheMuppets/proprietary_vendor_xiaomi_zeus.git -b lineage-22.2 vendor/xiaomi/zeus
+
+echo "Cloning Kernel"
+
+git lfs clone git@github.com:LineageOS/android_kernel_xiaomi_sm8450.git -b lineage-22.2 kernel/xiaomi/sm8450
+
+git lfs clone git@github.com:LineageOS/android_kernel_xiaomi_sm8450-modules.git -b lineage-22.2 kernel/xiaomi/sm8450-modules
+
+git lfs clone git@github.com:LineageOS/android_kernel_xiaomi_sm8450-devicetrees.git -b lineage-22.2 kernel/xiaomi/sm8450-devicetrees
+
+echo "Cloning MIUI Camera"
+
+git lfs clone https://gitlab.com/zeus-junk-yard/android_vendor_xiaomi_miuicamera-zeus.git -b 13 vendor/xiaomi/miuicamera-zeus
